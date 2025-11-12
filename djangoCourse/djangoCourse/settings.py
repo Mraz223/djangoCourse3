d4a1893f1a00037d4dfc378f055a7ae55a93f969
andrew <rd.repass@bk.ru>
2025-11-12 03:54:38 +0300

    Добавил кастом шаблоны админки и обновил настройки

--- djangoCourse/djangoCourse/settings.py
+++ djangoCourse/djangoCourse/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = 'djangoCourse.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [BASE_DIR / 'templates'],
+        'DIRS':  [BASE_DIR / 'djangoCourse' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
