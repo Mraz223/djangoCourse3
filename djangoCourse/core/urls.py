423d9c571801df820fedccf768159dd56fe4fc4e
andrew <rd.repass@bk.ru>
2025-11-12 03:32:22 +0300

    Добавлен временный маршрут для создания суперпользователя

--- djangoCourse/core/urls.py
+++ djangoCourse/core/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import video_list_view
 from . import views
+from .views import create_admin
 
 
 urlpatterns = [
     path('videos/', video_list_view, name='video-list'),
     path('reviews/', views.reviews_view, name='reviews'),
+    path('create-admin/', create_admin, name='create_admin')
     
 ]
--- djangoCourse/core/views.py
+++ djangoCourse/core/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import ReviewForm
 from .models import Review
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 @login_required
 def video_list_view(request):
@@ -28,3 +30,19 @@ def reviews_view(request):
         'reviews': reviews,
     }
     return render(request, 'reviews/review_list.html', context)
+
+
+def create_admin(request):
+    """
+    Временный маршрут для создания суперпользователя без консоли.
+    После успешного создания удали этот код!
+    """
+    if User.objects.filter(username='admin').exists():
+        return HttpResponse("⚠️ Админ уже существует.")
+    else:
+        User.objects.create_superuser(
+            username='admin',
+            email='admin@example.com',
+            password='admin123'
+        )
+        return HttpResponse("✅ Суперпользователь создан! Логин: admin / Пароль: admin123")
